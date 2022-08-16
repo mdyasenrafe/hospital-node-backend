29868b3c7d13781ee3967495df892f417678e6b3
Md yasen rafe <mdyasenrafe@gmail.com>
2022-08-17 02:04:13 +0600

    bug resolve

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -5,10 +5,11 @@ const { phone } = require("phone");
 
 exports.registrationUser = async (req, res) => {
   let password = req?.body?.password;
+  let hashedPassword;
   let pattern = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?!.* ).{8,16}$/;
   let passwordValidation = pattern.test(password);
   if (passwordValidation) {
-    let hashedPassword = await bcrypt.hash(password, 10);
+    hashedPassword = await bcrypt.hash(password, 10);
   }
   let email = req.body.email;
   let emailValidator = validator.isEmail(email);
